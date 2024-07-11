2d1c12c3233ab2ac1aaa0512556cc998c0588452
Cameron Poehner <cameronpoehner@gmail.com>
2024-07-10 19:40:40 -0700

    feat: setup client-side validation and form inputs

--- src/app/RegistrationForm.tsx
+++ src/app/RegistrationForm.tsx
@@ -1,15 +1,25 @@
 'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import { schema } from './registrationSchema';
 
+type OurSchema = z.infer<typeof schema>;
+
 export const RegistrationForm = () => {
-  const form = useForm<{
-    first: string;
-    last: string;
-    email: string;
-  }>({
+  const form = useForm<OurSchema>({
     resolver: zodResolver(schema),
     defaultValues: {
       first: '',
@@ -17,4 +27,60 @@ export const RegistrationForm = () => {
       email: '',
     },
   });
+
+  const onSubmit = async (data: OurSchema) => {
+    console.log('Form data:', data);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="first"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your first name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your last name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>Your email address.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import { RegistrationForm } from './RegistrationForm';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="mx-auto max-w-xl">
+      <RegistrationForm />
+    </div>
+  );
 }
